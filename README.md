312e73450c8ba77c68933ecf8fe35bef1994cec1
syrotkin <syrotkin@gmail.com>
2020-10-30 18:58:36 +0100

    end of lesson 05: useState Part 2

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 This is me following the tutorial from here: https://github.com/leveluptuts/hooks-starter
+
+Run
+
+```npm install```
+
+```npm start```
+
+If you get a security exception, use `serviceWorker.register` instead of `unregister`:
+
+https://stackoverflow.com/a/58727848
--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Toggle from './Toggle';
 
 const App = () => {
+
+  // template: how to use useState
+  // const [value, setValue] = useState(initialState);
+  const [name, setName] = useState('');
+
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
+      {/* coming up with a controlled input */}
+      <Toggle />
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        formSubmit(name, setName);
+      }}>
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
+};
+
+const formSubmit = (value, setValue) => {
+  console.log(`email sent to ${value}!`);
+  // Call to setValue clears the input 
+  // although called from a different function!
+  setValue('');
 };
 
 export default App;
