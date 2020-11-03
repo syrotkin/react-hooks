c5b80d34314a753ea6761d2c088e2595565a0d5a
syrotkin <syrotkin@gmail.com>
2020-11-03 21:50:44 +0100

    end of lesson 13: fetching data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useMemo } from "react";
+import React, { useRef, useState, useEffect, useMemo } from "react";
 import Toggle from "./Toggle";
 import useTitleInput from './hooks/useTitleInput';
 import Counter from './Counter';
@@ -7,22 +7,29 @@ const App = () => {
 
   // template: how to use useState
   // const [value, setValue] = useState(initialState);
-  const [title, setTitle] = useTitleInput('React Hooks (custom title)');   
+  const [title, setTitle] = useTitleInput('');   
 
   const ref = useRef();
   
-  const reverseWord = (word) => {
-    console.log("function called");
-    return [...word].reverse().join(""); 
+  const [dishes, setDishes] = useState([]);
+
+  const fetchDishes = async () => {
+    const response = await fetch('https://my-json-server.typicode.com/leveluptuts/fakeapi/dishes');
+    const data = await response.json();
+    console.log('fetched: ', data);
+    setDishes(data);
   };
 
-  const header = 'Level Up Dishes';
+  // this works:
+  // useMemo(() => fetchDishes(), [dishes.length]);
 
-  const HeaderReversed = useMemo(() => reverseWord(title), [title]);
+  useEffect(() => {
+    // don't need?
+  });
 
   return (
     <div className="main-wrapper" ref={ref}>
-      <h1>{HeaderReversed}</h1>
+      <h1>Level Up Dishes</h1>
       <Toggle />
       <Counter />
       <form
@@ -34,9 +41,21 @@ const App = () => {
           type="text"
           onChange={(e) => setTitle(e.target.value)}
           value={title}
-         />
+        />
         <button type="submit">Submit</button>
       </form>
+
+      {dishes.map((dish) => (
+        <article className="dish-card dish-card--withImage">
+          <h3>{dish.name}</h3>
+          <p>{dish.desc}</p>
+          <div className="ingredients">
+            {dish.ingredients.map((ingredient) => (
+              <span>{ingredient}</span>
+            ))}
+          </div>
+        </article>
+      ))}
     </div>
   );
 };
